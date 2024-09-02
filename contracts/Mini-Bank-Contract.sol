d41917b9eee604ac17233838530537b9d998772d
Jasid72 <Jasidhassan@gmail.com>
2024-09-03 04:35:50 +0500

    assert and modifer added

--- contracts/Mini-Bank-Contract.sol
+++ contracts/Mini-Bank-Contract.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.5;
 
 contract bank{
     mapping(address => uint) balance;
+    address owner;
+    modifier onlyowner{
+        require(owner == msg.sender, "Yor are not the owner");
+        _;
+    }
+    constructor(){
+        owner = msg.sender;
+    }
     function addBalance(uint _toAdd) public returns (uint) {
         balance[msg.sender] += _toAdd;
         return balance[msg.sender];
@@ -12,11 +20,14 @@ contract bank{
         return balance[msg.sender];
     }
 
-    function transfer(address reciver, uint amount) public {
+    function transfer(address reciver, uint amount) public onlyowner{
         require(balance[msg.sender] >= amount, "!!!!!!!!!!!!Not enough Balance!!!!!!!!!");
         require(msg.sender != reciver, "You are using same address. Change sender address.");
+
+        uint preBalance = balance[msg.sender];
         balance[msg.sender] -= amount;
         balance[reciver] += amount;
+        assert(balance[msg.sender] == preBalance - amount);
     }
 
 }
