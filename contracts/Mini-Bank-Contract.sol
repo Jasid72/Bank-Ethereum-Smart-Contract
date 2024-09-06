d3e6f19f9d79b2fb12923469afeddc6404ad88bf
Jasid72 <Jasidhassan@gmail.com>
2024-09-07 02:50:36 +0500

    Add payable function

--- contracts/Mini-Bank-Contract.sol
+++ contracts/Mini-Bank-Contract.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
+
+// Create a fucntion for find total balance of contract.
 pragma solidity ^0.8.5;
 
 contract bank{
-    mapping(address => uint) public  balance;
+    mapping(address => uint)  balance;
     address owner;
     modifier onlyowner{
         require(owner == msg.sender, "Yor are not the owner");
@@ -13,14 +15,16 @@ contract bank{
     }
 
     event balanceAdded(uint amount, address depositedTo);
-    
-    function addBalance(uint _toAdd) public returns (uint) {
-        balance[msg.sender] += _toAdd;
-        emit balanceAdded(_toAdd, msg.sender);
+
+    function deposite() public payable  returns (uint) {
+        balance[msg.sender] += msg.value;
+        emit balanceAdded(msg.value, msg.sender);
         return balance[msg.sender];
 
     }
-    
+    function getBalance() public view returns (uint){
+        return balance[msg.sender];
+    }
 
     function transfer(address reciver, uint amount) public onlyowner{
         require(balance[msg.sender] >= amount, "!!!!!!!!!!!!Not enough Balance!!!!!!!!!");
