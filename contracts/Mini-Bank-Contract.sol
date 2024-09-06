a564040ff973cbfa65d8a4f8b095b258d920f7fc
Jasid72 <Jasidhassan@gmail.com>
2024-09-07 02:12:23 +0500

    Get Amount Function removed

--- contracts/Mini-Bank-Contract.sol
+++ contracts/Mini-Bank-Contract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.5;
 
 contract bank{
-    mapping(address => uint) balance;
+    mapping(address => uint) public  balance;
     address owner;
     modifier onlyowner{
         require(owner == msg.sender, "Yor are not the owner");
@@ -11,14 +11,16 @@ contract bank{
     constructor(){
         owner = msg.sender;
     }
+
+    event balanceAdded(uint amount, address depositedTo);
+    
     function addBalance(uint _toAdd) public returns (uint) {
         balance[msg.sender] += _toAdd;
+        emit balanceAdded(_toAdd, msg.sender);
         return balance[msg.sender];
 
     }
-    function getBalance() public view returns (uint){
-        return balance[msg.sender];
-    }
+    
 
     function transfer(address reciver, uint amount) public onlyowner{
         require(balance[msg.sender] >= amount, "!!!!!!!!!!!!Not enough Balance!!!!!!!!!");
